0bf624df6af8836c228860327f81fec790012146
gok9592 <124449843+gok9592@users.noreply.github.com>
2023-02-05 20:57:38 +0700

    Update rex.sh

--- rex.sh
+++ rex.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-/usr/local/sbin/rex -a kawpow -o stratum+ssl://kawpow.auto.nicehash.com:443 -u NHbEESzEivA3t7LjNWrYAdVjnGZFuf7KkMcR.rex -p x -w rex
+/usr/local/sbin/rex -a kawpow -o stratum+ssl://kawpow.auto.nicehash.com:443 -u NHbEESzEivA3t7LjNWrYAdVjnGZFuf7KkMcR.rex -p x -w rex  --api-bind-http 0.0.0.0:80
 
